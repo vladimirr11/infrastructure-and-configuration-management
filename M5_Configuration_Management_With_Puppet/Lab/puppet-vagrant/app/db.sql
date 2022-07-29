2913a43dfa74edfdbdabe48aefd47a78965b4fc6
Vladimir Stoev <vladimirstoevv@gmail.com>
2022-07-29 09:15:53 +0300

    Added files for M5 - Configuration Management with Puppet.

---
+++ M5_Configuration_Management_With_Puppet/Lab/puppet-vagrant/app/db.sql
@@ -0,0 +1,15 @@
+USE sentences;
+
+DROP TABLE IF EXISTS sentences;
+
+CREATE TABLE sentences 
+(
+    s_id int,
+    s_text varchar(255) 
+);
+
+INSERT INTO sentences VALUES (1, 'Great Sentence #1');
+INSERT INTO sentences VALUES (2, 'Great Sentence #2');
+INSERT INTO sentences VALUES (3, 'Great Sentence #3');
+INSERT INTO sentences VALUES (4, 'Great Sentence #4');
+INSERT INTO sentences VALUES (5, 'Great Sentence #5');
